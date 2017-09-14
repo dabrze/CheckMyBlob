6516920ff68a9b005f1b05ca6066a4b9d1e4a638
Marcin Kowiel <mkowiel@ump.edu.pl>
2017-09-14 16:46:15 +0200

    pymol png

--- Figures/pymol/draw_3wh1_bonds.py
+++ Figures/pymol/draw_3wh1_bonds.py
@@ -1,7 +1,8 @@
 import sys, os
 sys.path.append(os.path.dirname(__file__))
-sys.path.append('D:\\VMs\kubuntu\VMSHARE\cmb_examples')
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'pymol_data'))
 print(os.path.abspath(__file__))
+print(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'pymol_data')))
 
 from draw_script import *
 
