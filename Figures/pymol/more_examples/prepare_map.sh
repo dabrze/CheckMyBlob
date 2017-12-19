b82ed1fc5e24213878e6b31b29d0bc9a3c2b8ad3
Marcin Kowiel <mkowiel@ump.edu.pl>
2017-12-19 20:36:09 +0100

    2pdt

--- Figures/pymol/more_examples/prepare_map.sh
+++ Figures/pymol/more_examples/prepare_map.sh
@@ -1,5 +1,5 @@
 
-for x in "1fpx_SAM_1699_A/1fpx" "1yqs_GOL_502_A/1yqs" "3a0k_ABA_240_G/3a0k" "4rk3_GOL_401_A/4rk3" "5aou_PO4_5001_A/5aou" "1kcz_EDO_903_A/1kcz" "1yuk_NDG_463_B/1yuk" "3s1y_IPA_400_A/3s1y" "4xfw_ACY_304_A/4xfw" "5fle_FES_1002_X/5fle" "1kwn_TAR_501_A/1kwn" "2e9m_GAL_1402_A/2e9m" "4j4z_PEG_203_A/4j4z" "4xgo_GOL_1008_A/4xgo"; do
+for x in "1fpx_SAM_1699_A/1fpx" "1yqs_GOL_502_A/1yqs" "3a0k_ABA_240_G/3a0k" "4rk3_GOL_401_A/4rk3" "5aou_PO4_5001_A/5aou" "1kcz_EDO_903_A/1kcz" "1yuk_NDG_463_B/1yuk" "3s1y_IPA_400_A/3s1y" "4xfw_ACY_304_A/4xfw" "5fle_FES_1002_X/5fle" "1kwn_TAR_501_A/1kwn" "2e9m_GAL_1402_A/2e9m" "4j4z_PEG_203_A/4j4z" "4xgo_GOL_1008_A/4xgo" "2pdt_FAD_204_D/2pdt"; do
 echo "Run $x"
 pdb="$x.pdb"
 mtz="$x""_refmac.mtz"
